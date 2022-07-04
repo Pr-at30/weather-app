627e68a1b40ed6173d8474be0b5f5dc9db06db2d
Pratyush Jena <pratyushjena8@gmail.com>
2022-07-04 20:00:15 +0530

    Update

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -2,21 +2,37 @@ import React from 'react'
 import style from './Display.module.css'
 
 const Display = (props) => {
-  let celtemp = (props.weatherData?.main?.temp-273.15).toFixed(2);;
+  console.log(props)
+  let celtemp = (props.weatherData?.main?.temp - 273.15).toFixed(2);
+  let feelike = (props.weatherData?.main?.feels_like - 273.15).toFixed(2);
   return (
     <div className={style.widget}>
 
-      {props.error === true ? <h1>Nothing to show</h1> : <div className={style.panel}>
-        <div className={style.date}>
-          {new Date().toLocaleDateString()}
-        </div>
-        <div className={style.city}>
-          {props.weatherData?.name}, {props.weatherData?.sys?.country}
-        </div>
-        <div className={style.temp}>
-          {celtemp}&deg;C
-        </div>
-      </div>}
+      {props.error === true ?
+        
+        <h1>Nothing to show</h1> :
+        <>
+          <div className={style.panel}>
+            <div className={style.date}>
+              {new Date().toLocaleDateString()}
+            </div>
+            <div className={style.city}>
+              {props.weatherData?.name}, {props.weatherData?.sys?.country}
+            </div>
+            <div className={style.temp}>
+              Temp : {celtemp}&deg;C
+            </div>
+            <div className={style.temp}>
+              Feels like : {feelike}&deg;C
+            </div>
+            <div className={style.temp}>
+            Desc : {props.weatherData?.weather[0].description}
+            </div>
+          </div>
+
+          
+        </>
+      }
 
     </div>
   )
@@ -24,14 +40,14 @@ const Display = (props) => {
 
 export default Display
 
-{/* <div className={style.panel}>
-          <div className={style.date}>
-            Monday, 20 Aug 2018
-          </div>
-          <div className={style.city}>
-            Mumbai
-          </div>
-          <div className={style.temp}>
-            27&deg;
-          </div>
-        </div> */}
+//  <div className={style.panel}>
+//           <div className={style.date}>
+//             Monday, 20 Aug 2018
+//           </div>
+//           <div className={style.city}>
+//             Mumbai
+//           </div>
+//           <div className={style.temp}>
+//             27&deg;
+//           </div>
+//         </div> 
